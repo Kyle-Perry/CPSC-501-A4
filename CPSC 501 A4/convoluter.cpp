bfa84adc60865a806e7b57817393e668fc146635
Kyle Perry <perry.246@gmail.com>
2023-11-21 17:07:08 -0700

    Renamed file to convoluter
    Renamed CPSC 501 A4.cpp to convoluter

--- CPSC 501 A4/convoluter.cpp
+++ CPSC 501 A4/convoluter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 
+
 struct wavHeader {
     char chunkID[4];
     uint32_t chunkSize;
@@ -21,14 +22,30 @@ struct wavHeader {
 int main(int argc, char *argv[])
 {
      FILE* sourceFile = NULL;
-     FILE* impulseResponseFile = NULL;
+     FILE* irFile = NULL;
 
      if (argc == 3)
      {
+         sourceFile = fopen(argv[1], "r");
+         if (sourceFile == NULL)
+         {
+             std::cerr << "Error: Failed to open file \"" << argv[1] << "\"" << std::endl;
+             exit(-1);
+         }
+
+         irFile = fopen(argv[2], "r");
+         if (irFile == NULL)
+         {
+             std::cerr << "Error: Failed to open file \"" << argv[2] << "\"" << std::endl;
+             exit(-1);
+         }
+
 
      }
      else
      {
          std::cerr << "Error: Incorrect number of arguments given. Proper usage is \"" << argv[0] << "\" sourceFilename impulseResponseFilename" << std::endl;
+         exit(-1);
      }
+     return 0;
 }
